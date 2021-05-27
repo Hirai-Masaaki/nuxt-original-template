e44815ee8221eb8da34e799efe95f4501b1bc248
Hirai-Masaaki <60721915+Hirai-Masaaki@users.noreply.github.com>
2021-05-27 09:30:19 +0900

    ヘッダー作成

--- components/header.vue
+++ components/header.vue
@@ -1,5 +1,12 @@
 <template>
-  <header class="header">
-    <h1 class="header-title"><n-link to="/">nuxt-original-template</n-link></h1>
+  <header class="l-header c-flex c-flex--align-center c-flex--justify-end">
+    <div class="p-header-content c-flex c-flex--align-center">
+      <n-link to="/" class="c-link c-text--small c-text--bold p-header-content__item">Mail</n-link>
+      <n-link to="/" class="c-link c-text--small c-text--bold p-header-content__item">画像</n-link>
+    </div>
+    <div class="p-header-content c-flex c-flex--align-center">
+      <button class="c-button p-header-content__item"><img class="c-icon--small p-header-menu-button" src="~/assets/image/top_menu_icon.svg" alt="メニュー アイコン"/></button>
+      <button class="c-button p-header-content__item"><img class="p-profile-button" src="~/assets/image/jaga.jpg" alt="プロフィール画像"></button>
+    </div>
   </header>
 </template>
